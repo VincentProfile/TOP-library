cf10e7d3c759a8ecf5854d274cc8ca33c67ca2d6
vincentprofile <vincentccs93@gmail.com>
2021-03-21 16:01:52 +0800

    add edit book information button

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let myLibrary = [
         Genre: 'Biography, Nonfiction, Technology, Inspirational',
         Year: 2011,
         'Number of Pages': 500,
-        Read: true,
+        ReadStatus: 'Read',
         DateCompleted: '01-01-2021',
     },
     {
@@ -14,7 +14,7 @@ let myLibrary = [
         Genre: 'Biography, Nonfiction, Technology, Engineering',
         Year: 2017,
         'Number of Pages': 600,
-        Read: true,
+        ReadStatus: 'Read',
         DateCompleted: '01-01-2021',
 
     },
@@ -24,17 +24,17 @@ let myLibrary = [
         Genre: 'Biography, Nonfiction, Science',
         Year: 2007,
         'Number of Pages': 700,
-        Read: true,
+        ReadStatus: 'Read',
         DateCompleted: '01-01-2021',
     },
 ];
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const genre = document.querySelector('#genre');
-const year = document.querySelector('#year');
-const pages = document.querySelector('#pages');
-const readStatus = document.querySelector('#readStatus');
-const dateCompleted = document.querySelector('#completedDate');
+let title = document.querySelector('#title');
+let author = document.querySelector('#author');
+let genre = document.querySelector('#genre');
+let year = document.querySelector('#year');
+let pages = document.querySelector('#pages');
+let readStatus = document.querySelector('#readStatus');
+let dateCompleted = document.querySelector('#completedDate');
 
 function Book() {
     // constructor
@@ -46,32 +46,68 @@ Book.prototype.addBookToLibrary = () => {
         Author: author.value,
         Genre: genre.value,
         Year: year.value,
+        ['Number of Pages']: pages.value,
         ReadStatus: readStatus.value,
         DateCompleted: dateCompleted.value,
     })
 }
+
 // on submit form
 const form = document.querySelector('.form');
 form.onsubmit = () => {
-    const newBook = new Book();
-    newBook.addBookToLibrary();
+    if (myLibrary.find(book => book.Title === title.value)){
+        if (confirm("Update existing book information?")){
+            const bookNumber = myLibrary.findIndex(book => book.Title === title.value);
+            myLibrary[bookNumber].Title = title.value;
+            myLibrary[bookNumber].Author = author.value;
+            myLibrary[bookNumber].Genre = genre.value;
+            myLibrary[bookNumber].Year = year.value;
+            myLibrary[bookNumber]['Number of Pages'] = pages.value;
+            myLibrary[bookNumber].ReadStatus = readStatus.value;
+            myLibrary[bookNumber].DateCompleted = dateCompleted.value;
+
+            formModal.style.display = 'none'; 
+        }
+    }else{
+        const newBook = new Book();
+        newBook.addBookToLibrary();
+        formModal.style.display = 'none'; 
+    }
+
     books.innerHTML = '';
     showBooksOnLibrary();
-    formModal.style.display = 'none'; 
+
     // temporary measure to stay on current page
     return false;
 }
+
 const openModal = document.querySelector('#openModal');
 openModal.addEventListener('click', showForm);
 
 const formModal = document.querySelector('.modal');
-function showForm(){
+function showForm(e){
+    if (e.target.id == 'editForm'){
+        //const bookInfo = document.querySelectorAll(`[data-index="${e.target.getAttribute('data-index')}"]`);
+        myLibrary.forEach((book, index) =>{
+            if (e.target.getAttribute('data-index') == index){
+                title.value = book.Title;
+                author.value = book.Author;
+                genre.value = book.Genre;
+                year.value = book.Year;
+                pages.value = book['Number of Pages'];
+                readStatus.value = book.ReadStatus;
+                dateCompleted.value = book.DateCompleted;
+            }
+        })
+    }
+    
     formModal.style.display = 'block';
 }
 
 window.addEventListener('click', closeForm);
+window.addEventListener('keydown', closeForm);
 function closeForm(e){
-    if (e.target == formModal) {
+    if (e.target == formModal || e.keyCode == "27") {
         formModal.style.display = "none";
     }
 }
@@ -79,49 +115,86 @@ function closeForm(e){
 const books = document.querySelector('.books');
 
 function showBooksOnLibrary(){
-    myLibrary.forEach(book => {
+    myLibrary.forEach((book, index) => {
         const div = document.createElement('Div');
         div.className = 'card';
+        div.setAttribute('data-index', index);
 
         const title = document.createElement('h3');
         title.className = 'title';
+        title.setAttribute('data-index', index);
         title.innerText = book.Title;
 
         const author = document.createElement('p');
         author.className = 'author';
-        author.innerText = `Author: ${book.Author}`;
+        author.setAttribute('data-index', index);
+        if (book.Author != "")
+            author.innerText = `Author: ${book.Author}`;
 
         const genre = document.createElement('p');
         genre.className = 'genre';
-        genre.innerText = `Genre: ${book.Genre}`;
+        genre.setAttribute('data-index', index);
+        if (book.Genre != "")
+            genre.innerText = `Genre(s): ${book.Genre}`;
         
         const year = document.createElement('p');
         year.className = 'year';
-        year.innerText = `Year Published: ${book.Year}`;
+        year.setAttribute('data-index', index);
+        if (book.Year != "")
+            year.innerText = `Published In: ${book.Year}`;
         
         const pages = document.createElement('p');
-        pages.className = 'year';
-        pages.innerText = `${book['Number of Pages']} Pages`;
+        pages.className = 'pages';
+        pages.setAttribute('data-index', index);
+        if (book['Number of Pages'] != "")
+            pages.innerText = `${book['Number of Pages']} Pages`;
 
-        const read = document.createElement('p');
-        read.className = 'read';
-        read.innerText = book.Read;
+        const readStatus = document.createElement('p');
+        readStatus.className = 'readStatus';
+        readStatus.innerText = `Status: ${book.ReadStatus}`;
+
+        const dateCompleted = document.createElement('p');
+        dateCompleted.className = 'dateCompleted';
+        if (book.ReadStatus == "Read")
+            dateCompleted.innerText = `Completed On: ${book.DateCompleted}`;
+        dateCompleted.setAttribute('data-index', index);
+
+        const btnsDiv = document.createElement('div');
+        btnsDiv.className = 'div';
+        btnsDiv.setAttribute('data-index', index);
+
+        const editFormBtn = document.createElement('button');
+        editFormBtn.className = 'button';
+        editFormBtn.id = 'editForm';
+        editFormBtn.innerText = 'Edit Book';
+        editFormBtn.setAttribute('data-index', index);
+        editFormBtn.addEventListener('click', showForm);
 
         const removeBookBtn = document.createElement('button');
         removeBookBtn.className = 'button';
         removeBookBtn.id = 'removeBook';
         removeBookBtn.innerText = 'Remove Book';
+        removeBookBtn.setAttribute('data-index', index);
+        removeBookBtn.addEventListener('click', removeBook);
+
+        btnsDiv.appendChild(editFormBtn);
+        btnsDiv.appendChild(removeBookBtn);
 
         div.appendChild(title);
         div.appendChild(author);
         div.appendChild(genre);
-        div.append(year);
-        div.append(pages);
-        div.append(read);
-        div.append(removeBookBtn);
+        div.appendChild(year);
+        div.appendChild(pages);
+        div.appendChild(readStatus);
+        div.appendChild(dateCompleted);
+        div.appendChild(btnsDiv);
 
         books.appendChild(div);
     })
+
+    function removeBook(e){
+
+    }
 }
 
 showBooksOnLibrary();
